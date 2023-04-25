cf8eec3e4fadb0c3015f085da91eb910c1360fc9
Damian Montero <photoca@hotmail.com>
2023-04-25 14:33:30 -0400

    latest typescript work

---
+++ index.ts
@@ -0,0 +1,43 @@
+let age : number = 23
+let firstName: string = "Damian"
+let isTall: boolean = true;
+let childrensNames: string[] = ["Lily","David"]
+
+//console.log(`${firstName} is ${age} years old`)
+if (isTall) {
+   // console.log("and so tall")
+} else {
+    //console.log("and so pretty")
+}
+
+for (let i: number = 0; i < childrensNames.length;i++) {
+    //console.log(childrensNames[i])
+}
+
+
+
+enum taxForm {
+    standardTaxForm = "1040",
+    childTaxForm = "641",
+    cryptTaxForm = "420S"
+}
+
+// console.log("the crypto one is: ", taxForm.cryptTaxForm)
+// console.log("the crypto one is: ", taxForm.cryptTaxForm)
+// console.log("the crypto one is: ", taxForm.cryptTaxForm)
+// console.log("the child one is: ", taxForm.childTaxForm)
+
+
+let zipCode: string | number = 12345 // good
+zipCode = "ABC" //good 
+//zipCode = null //bad
+// console.log(zipCode)
+
+function sum(x: number, y: number): number {
+    return x+y
+}
+
+let total: number =  sum(5,6)
+console.log(total)
+
+
